ad0ab091099f379cd713d0e5d8429e786c1045b6
yueming xu <xnysym@outlook.com>
2018-01-06 23:52:30 +0800

    初始化工程

---
+++ YMHTTPClient/YMHTTPClient/YMHttpClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YMHTTPClient
+{
+    public static class YMHttpClient
+    {
+        public static void Post()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            var content = new FormUrlEncodedContent(new Dictionary<string, string>()
+            {
+               {"ak", "65fc7ca4fc441d26f71bf3d691b278c2"},
+               {"deviceId", "537eb34be4b022b7fbe19471"}
+            });
+            client.PostAsync("", content);
+        }
+    }
+}
